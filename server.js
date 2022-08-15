98df0c2511cc12658897d73bf6a908664d978b75
Laetitia Goncalves <laetitiagoncalves@macbook-pro-de-laetitia.home>
2022-08-15 10:35:12 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const cors = require("cors");
+const axios = require("axios");
+require("dotenv").config();
+
+const app = express();
+app.use(cors());
+
+app.use(express.json());
+
+const key = process.env.API_KEY;
