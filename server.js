4ed4e985ca1e271a625c8be2c06cc54f34251518
Laetitia Goncalves <laetitiagoncalves@macbook-pro-de-laetitia.home>
2022-08-15 11:00:21 +0200

    adding routes

--- server.js
+++ server.js
@@ -9,3 +9,35 @@ app.use(cors());
 app.use(express.json());
 
 const key = process.env.API_KEY;
+
+// Homepage avec tous les jeux
+
+app.get("/", async (req, res) => {
+  try {
+    const url = `https://api.rawg.io/api/games?key=${key}`;
+    const response = await axios.get(url);
+    res.status(200).json(response.data);
+  } catch (error) {
+    res.status(400).json("route games not found");
+  }
+});
+
+// Route detail d'un jeu
+
+app.get("/games/:id", async (req, res) => {
+  try {
+    const url = `https://api.rawg.io/api/games/${id}?key=${key}`;
+    const response = await axios.get(url);
+    res.status(200).json(response.data);
+  } catch (error) {
+    res.status(400).json("route of the game not found");
+  }
+});
+
+app.all("*", (req, res) => {
+  res.status(404).json({ message: "route not found !" });
+});
+
+app.listen(process.env.PORT, () => {
+  console.log("Server has started");
+});
